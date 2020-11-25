16bc4719831eadfba9a03e953fa3eecb58239053
UpperLeaf <tkdduq8260@gmail.com>
2020-11-25 11:15:19 +0900

    Exception Handler를 RestControllerAdvice로 변경

--- src/main/java/com/econovation/whichbook_user/domain/user/service/UserService.java
+++ src/main/java/com/econovation/whichbook_user/domain/user/service/UserService.java
@@ -1,11 +1,9 @@
 package com.econovation.whichbook_user.domain.user.service;
 
-import com.econovation.whichbook_user.domain.user.User;
 import com.econovation.whichbook_user.domain.user.dto.LoginRequestDto;
 import com.econovation.whichbook_user.domain.user.dto.SignUpRequestDto;
 import com.econovation.whichbook_user.domain.user.dto.UserResponseDto;
 
-import java.util.Optional;
 
 public interface UserService {
 
